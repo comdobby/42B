547340d276fb7422bc47292787fd2d80afae8c64
comdobby <tofk1107@gmail.com>
2023-08-30 11:58:25 +0200

    edit bonus part

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: saeraryu <saeraryu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 10:59:39 by saeraryu          #+#    #+#             */
-/*   Updated: 2023/08/30 11:47:25 by saeraryu         ###   ########.fr       */
+/*   Updated: 2023/08/30 11:58:00 by saeraryu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new_node = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		new_list = ft_lstnew(f(lst->content));
+		if (!new_list)
 		{
-			ft_lstclear(&new_list, del);
+			ft_lstclear(&new_node, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_node);
+		ft_lstadd_back(&new_node, new_list);
 		lst = lst->next;
 	}
 	return (new_list);

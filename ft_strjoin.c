2392c76a8762755863518a37276501f8b18b9b3c
comdobby <tofk1107@gmail.com>
2023-11-23 16:03:50 +0100

    ft_join

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dobby <dobby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 17:27:53 by saryu             #+#    #+#             */
-/*   Updated: 2023/11/23 15:30:21 by dobby            ###   ########.fr       */
+/*   Updated: 2023/11/23 16:03:27 by dobby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	unsigned long	j;
 	char			*result;
 
-	if (!s1 && !s2)
-		return (NULL);
 	i = 0;
 	j = 0;
 	result = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!result)
 		return (NULL);
-	while (i < (ft_strlen(s1) + ft_strlen(s2)))
+	while (s1 && s1[i])
 	{
-		while (i < ft_strlen(s1))
-		{
-			result[i] = s1[i];
-			i++;
-		}
-		result[i] = s2[j];
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2 && s2[j])
+	{
+		result[j] = s2[j];
 		i++;
 		j++;
 	}
